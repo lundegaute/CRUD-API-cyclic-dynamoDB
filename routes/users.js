bdb033776c7f534c90b17e97e843e7a3b674335d
Gaute Lunde <lundegaute@gmail.com>
2024-01-25 16:46:29 +0100

    Added dotenv, cyclic.sh/dynamoDB, aws-sdk

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const CyclicDB = require('@cyclic.sh/dynamodb')
+const db = CyclicDB(process.env.CYCLIC_DB)
+let users = db.collection('users')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  const list = await users.list()
+  res.send(list);
 });
 
 module.exports = router;
